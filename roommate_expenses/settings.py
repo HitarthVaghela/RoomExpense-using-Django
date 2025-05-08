9e1e895bb82ddce147c8fe1023811b868311eb2b
HitarthVaghela <hdvaghela27@gmail.com>
2025-05-08 12:29:08 +0530

    Fixed

--- roommate_expenses/settings.py
+++ roommate_expenses/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6+3cdtc4d2c%lqtj+hu2gm3-lp&(b@j1s#sk@m)sx-g$=c&8p@'
+SECRET_KEY = 'django-insecure-6+3cdtc4d2c%lqtj+hu2gm3-lp&(b@j1s#sk@m)sx-g$=c&8p@@@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
